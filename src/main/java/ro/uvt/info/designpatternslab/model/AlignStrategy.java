07b456a3f47099e5f2b0748b50d13091430c08dc
student <safas@da>
2023-11-07 08:14:26 +0200

    lab 5 update :)

---
+++ src/main/java/ro/uvt/info/designpatternslab/model/AlignStrategy.java
@@ -0,0 +1,6 @@
+package ro.uvt.info.designpatternslab.model;
+
+
+public interface AlignStrategy {
+    public void render(Paragraph paragraph, Context context);
+}
---
+++ src/main/java/ro/uvt/info/designpatternslab/model/Context.java
@@ -0,0 +1,21 @@
+package ro.uvt.info.designpatternslab.model;
+
+
+public class Context {
+    private int pageWidth=156;
+
+    public Context() {
+    }
+
+    public Context(int pageWidth) {
+        this.pageWidth = pageWidth;
+    }
+
+    public int getPageWidth() {
+        return pageWidth;
+    }
+
+    public void setPageWidth(int pageWidth) {
+        this.pageWidth = pageWidth;
+    }
+}
